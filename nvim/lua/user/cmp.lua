e0a1c1b6891172d1bab3c2e1e52f9070baf68ebb
Reto Barmettler <reto.barmettler@sedimentum.com>
2022-08-22 11:31:16 +0200

    Change cmp-exit key to q

--- nvim/lua/user/cmp.lua
+++ nvim/lua/user/cmp.lua
@@ -58,7 +58,7 @@ cmp.setup {
     ["<C-l>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
     ["<C-h>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
     ["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
-    ["<esc>"] = cmp.mapping {
+    ["q"] = cmp.mapping {
       i = cmp.mapping.abort(),
       c = cmp.mapping.close(),
     },
