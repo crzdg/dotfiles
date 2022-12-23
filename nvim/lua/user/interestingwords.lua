6b011a45f339613fddd9764b4387c03e21092fad
Reto Barmettler <reto.barmettler@sedimentum.com>
2022-12-23 10:09:47 +0100

    nvim: Updated list of interestingwords colors

--- nvim/lua/user/interestingwords.lua
+++ nvim/lua/user/interestingwords.lua
@@ -1,14 +1,19 @@
 local colors = require("gruvbox-baby.colors").config()
-local cmd = string.format([[ let g:interestingWordsGUIColors = ["%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s"] ]],
+local cmd = string.format([[ let g:interestingWordsGUIColors = ["%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s"] ]],
     colors.forest_green,
     colors.magenta,
     colors.blue_gray,
     colors.bright_yellow,
     colors.error_red,
-    colors.clean_green,
+    colors.soft_green,
     colors.pink,
+    colors.light_blue,
     colors.soft_yellow,
-    colors.red
+    colors.red,
+    colors.clean_green,
+    colors.milk,
+    colors.dark_gray,
+    colors.orange
 )
 
 vim.cmd(cmd)
