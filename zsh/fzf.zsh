4fd3d855cce124d7f37b1ed75a7e2276a2c9bb83
Reto Barmettler <reto.barmettler@sedimentum.com>
2023-01-08 12:26:24 +0100

    fzf: Update fzf base theme

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -1,8 +1,35 @@
 
-export FZF_DEFAULT_OPTS="--height 80% --layout=reverse --border --info=inline --prompt '> '
-    --color fg:${FOREGROUND},bg:${BG},hl:${BRIGHT_YELLOW},fg+:${FOREGROUND},bg+:${COMMENT},hl+:${BRIGHT_YELLOW}
-    --color info:${DARK_GREY},prompt:#bdae93,spinner:${BRIGHT_YELLOW},pointer:#83a598,marker:#fe8019,header:#665c54
+FZF_THEME="
+--color=fg:${FOREGROUND} \
+--color=bg:${BACKGROUND} \
+--color=hl:${BRIGHT_YELLOW} \
+--color=fg+:bold:${FOREGROUND} \
+--color=bg+:${COMMENT} \
+--color=hl+:${BRIGHT_YELLOW} \
+--color=gutter:${BACKGROUND} \
+--color=info:${ORANGE} \
+--color=separator:${BACKGROUND} \
+--color=border:${MILK} \
+--color=label:${SOFT_YELLOW} \
+--color=prompt:${LIGHT_BLUE} \
+--color=spinner:${BRIGHT_YELLOW} \
+--color=pointer:bold:${BRIGHT_YELLOW} \
+--color=marker:${ERROR_RED} \
+--color=header:${ORANGE} \
+--color=preview-fg:${FOREGROUND} \
+--color=preview-bg:${BACKGROUND} \
 "
+
+export FZF_DEFAULT_OPTS="
+--ansi \
+--height 80% \
+--layout=reverse \
+--border bold \
+--info=inline \
+--prompt '> ' \
+${FZF_THEME}
+"
+
 export FZF_CTRL_R_OPTS="--preview 'echo {}' --preview-window down:3:hidden:wrap --bind '?:toggle-preview'"
 
 export FZF_COMPLETION_TRIGGER='**'
