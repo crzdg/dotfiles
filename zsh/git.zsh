ddea31998e6a6ce362cc7c2da98b18c30c0fe887
Reto Barmettler <reto.barmettler@sedimentum.com>
2023-01-16 13:35:56 +0100

    zsh: Move github functions to git.zsh

--- zsh/git.zsh
+++ zsh/git.zsh
@@ -14,7 +14,6 @@ _go_to_toplevel_if_git_dir () {
     fi
 }
 
-
 gbrowse() {
   _gitLogLineToHash="echo {} | grep -o '[a-f0-9]\{7\}' | head -1 | tr -d '\n'"
   _viewGitLogLine="$_gitLogLineToHash | xargs -I % sh -c 'git show --color=always %'"
@@ -25,4 +24,23 @@ gbrowse() {
     --bind "enter:execute:$_viewGitLogLine | less -R" \
     --bind "alt-y:execute:$_gitLogLineToHash | tmux load-buffer -" \
     --bind "alt-c:execute:$_gitLogLineToHash | xclip -i -sel c"
+}
+
+_gh() {
+  _is_in_git_repo || return
+  git log --date=short --format="%C(green)%C(bold)%cd %C(auto)%h%d %s (%an)" --graph --color=always |
+    fzf --ansi --no-sort --reverse --multi --bind 'ctrl-s:toggle-sort' \
+    --header 'Press CTRL-S to toggle sort' \
+    --preview 'grep -o "[a-f0-9]\{7,\}" <<< {} | xargs git show --color=always' |
+    grep -o "[a-f0-9]\{7,\}"
+}
+
+_gb() {
+  _is_in_git_repo || return
+  git branch -a --color=always | grep -v '/HEAD\s' | sort |
+    fzf --height 100% --ansi --multi --tac --preview-window right:70% \
+    --preview 'git log --oneline --graph --date=short --color=always --pretty="format:%C(auto)%cd %h%d %s" $(sed s/^..// <<< {} | cut -d" " -f1)' \
+    --preview-window=down:28:wrap |
+    sed 's/^..//' | cut -d' ' -f1 |
+    sed 's#^remotes/##'
 }
