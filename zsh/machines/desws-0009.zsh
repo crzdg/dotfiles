4a6a310a441cb7ea5fc78e1293a7c278fc6e3be2
crzdg <reto@fambarm.ch>
2023-02-26 10:15:22 +0100

    zsh,tmux: refactor host indication

--- zsh/machines/desws-0009.zsh
+++ zsh/machines/desws-0009.zsh
@@ -2,3 +2,10 @@ export PATH="$PATH:$HOME/dev/balena-cli" # Add balena cl to path
 
 alias csedi="openvpn-gui.exe --command silent_connection 1 && openvpn-gui.exe --command connect DESWS-0009"
 alias dsedi="openvpn-gui.exe --command disconnect DESWS-0009"
+
+# export HOST_INDICATOR_COLOR="#732f37" # sedimentum magenta
+export HOST_INDICATOR_COLOR=$MAGENTA
+HOST_EMOJI="🪨"
+HOST_USER_SUFFIX="🦄🚀"
+SPACESHIP_HOST_SHOW="false"
+SPACESHIP_VENV_SYMBOL="🌍 "
--- zsh/machines/moltres.zsh
+++ zsh/machines/moltres.zsh
@@ -1,6 +1,6 @@
 alias wudo="/usr/bin/python3 ~/dev/wsl-sudo/wsl-sudo.py"
 
-ZSH_THEME="spaceship"
-source $ZSH_CONFIG_DIR/prompts/spaceship_prompt_moltres.zsh
-plugins=()
-
+export HOST_INDICATOR_COLOR=$ORANGE
+HOST_EMOJI="🔥"
+HOST_USER_SUFFIX="🐦🫕"
+SPACESHIP_HOST_SHOW="false"
