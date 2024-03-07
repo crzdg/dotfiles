2e267f93af2cea28f8c6eb11d89783baa1c9b1d7
Reto Barmettler <reto.barmettler@helpany.com>
2024-03-07 07:50:22 +0100

    zsh: Sedimentum -> Helpany

--- zsh/machines/desws-0009.zsh
+++ zsh/machines/desws-0009.zsh
@@ -1,16 +1,32 @@
-export PATH="$PATH:$HOME/dev/balena-cli" # Add balena cl to path
-
 alias csedi="openvpn-gui.exe --command silent_connection 1 && openvpn-gui.exe --command connect DESWS-0009"
 alias dsedi="openvpn-gui.exe --command disconnect DESWS-0009"
 
-# HOST_INDICATOR_COLOR="#732f37" # sedimentum magenta
-HOST_INDICATOR_COLOR=$MAGENTA
-HOST_EMOJI="🚀"
-HOST_USER_SUFFIX="🦄🪨"
-SPACESHIP_HOST_SHOW="false"
-SPACESHIP_VENV_SYMBOL="🌍 "
-
-PROJECTX_FOLDERS="~/git ~/git/sedimentum"
+PROJECTX_FOLDERS="~/git ~/git/helpany"
 PROJECTX_INTERNET_CHECKS=1
 PROJECTX_GIT_FETCH=1
 PROJECTX_BUMP_PYTHON=0
+
+# Helpany
+# Lightest Blue: #28d2e6
+# Lightest Pink: #ff788c
+# Text Blue: #000787
+HELPANY_LIGHT_BLUE="#28d2e6"
+HELPANY_LIGHT_PINK="#ff788c"
+HOST_INDICATOR_COLOR="#28d2e6"
+HOST_INDICATOR_COLOR=$HELPANY_LIGHT_PINK
+HOST_EMOJI="🚀"
+# HOST_USER_SUFFIX="🩵🩷"
+# HOST_USER_SUFFIX="\U1FA75\U1FA77"
+HOST_USER_SUFFIX="💙💗"
+SPACESHIP_HOST_SHOW="false"
+SPACESHIP_VENV_SYMBOL="🪨 "
+SPACESHIP_GIT_BRANCH_COLOR=$HELPANY_LIGHT_PINK
+SPACESHIP_GIT_STATUS_COLOR=$HELPANY_LIGHT_BLUE
+
+# Sedimentum Legacy
+# SEDIMENTUM_MAGENTA="#732f37"
+# HOST_INDICATOR_COLOR=$MAGENTA
+# HOST_EMOJI="🚀"
+# HOST_USER_SUFFIX="🦄🪨"
+# SPACESHIP_HOST_SHOW="false"
+# SPACESHIP_VENV_SYMBOL="🌍 "
